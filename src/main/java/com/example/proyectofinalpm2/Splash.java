e883b717a788481756a838acaaef278d54e79454
erlan vedia <erlan.vediaherrera@gmail.com>
2020-11-23 10:58:03 -0400

    version 1

---
+++ src/main/java/com/example/proyectofinalpm2/Splash.java
@@ -0,0 +1,31 @@
+package com.example.proyectofinalpm2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash extends Activity {
+    private final int tiempo=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent c=new Intent(Splash.this,MainActivity.class);
+                startActivity(c);
+                finish();
+            };
+        },tiempo);
+    }
+}
